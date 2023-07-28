f64879b10d7a8f239ce041f5ca0f6caae2e2d9c2
ART0 <18333981+0Art0@users.noreply.github.com>
2023-07-28 17:33:30 +0530

    UI enhancements

--- widget/src/motivatedProofUI.tsx
+++ widget/src/motivatedProofUI.tsx
@@ -1,35 +1,57 @@
 import * as React from 'react';
 import { Position, Range, TextEdit, WorkspaceEdit } from 'vscode-languageserver-types';
-import { EditorContext, RpcContext, useAsync } from '@leanprover/infoview';
-import { Button } from '@mui/material';
+import { DocumentPosition, EditorContext, RpcContext, useAsync } from '@leanprover/infoview';
+import { Typography, Button, Grid } from '@mui/material';
 
-function insertText(props:{pos:Position, text: String}) {
+/** Inserts the text at the specified position by calling `makeInsertCommand` from the Lean file,
+ *  and switches to a new cursor position.
+ */
+function insertText(props:{pos:DocumentPosition, text: String}) {
   const rpcSession = React.useContext(RpcContext)
   const editorCtx = React.useContext(EditorContext)
-  const asyncWSEdit = useAsync<WorkspaceEdit>(async () => {
+  const asyncWSEdit = useAsync<{edit:WorkspaceEdit, newPos:DocumentPosition}>(async () => {
     return await rpcSession.call('makeInsertionCommand', props)
   }, [])
 
   return () => {
     return void (async () => {
       if (asyncWSEdit.state === "resolved") {
-        await editorCtx.api.applyEdit(asyncWSEdit.value);
+        await editorCtx.api.applyEdit(asyncWSEdit.value.edit);
+        await editorCtx.revealPosition({...asyncWSEdit.value.newPos, uri: props.pos.uri});
       }
     })()
   };
 };
 
-function InsertionButton(props:{pos:Position, label:String, text:String}): JSX.Element {
-  return <Button onClick={insertText({pos: props.pos, text: props.text})}>{props.label}</Button>;
+/** A Button UI component that calls the `insertText` function when clicked. */
+function InsertionButton(props:{pos:DocumentPosition, label:String, text:String}): JSX.Element {
+  return (<Button
+    variant="outlined"
+    color="primary"
+    size="medium"
+    style={{ textTransform: 'none' }}
+    onClick={insertText({pos: props.pos, text: props.text})}>
+      {props.label}
+      </Button>);
 }
 
-export default function motivatedProofPanel(props:{pos:Position, buttons:{label:String, text:String}[]}) {
+/** The main UI panel for displaying motivated proofs. */
+export default function motivatedProofPanel
+    (props:{pos:DocumentPosition, buttons:{label:String, text:String}[]}) {
   return (
   <details open={true}>
     <summary>Motivated proof interface</summary>
-    { props.buttons.map ((button:{label:String, text:String}) => {
-        return <InsertionButton pos={props.pos} label={button.label} text={button.text} />;
-      }) }
+    <Typography variant="button" display="block" mt={2} gutterBottom>
+      Proof-generating moves
+    </Typography>
+    <Grid container direction="column" spacing={1}>
+      { props.buttons.map ((button:{label:String, text:String}) => {
+          return (
+            <Grid item>
+            <InsertionButton pos={props.pos} label={button.label} text={button.text} />
+            </Grid>);
+        }) }
+    </Grid>
   </details>
   );
 }
