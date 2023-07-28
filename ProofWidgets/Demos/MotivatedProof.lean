ff665c1e2970256a595af17be1802633c644b797
ART0 <18333981+0Art0@users.noreply.github.com>
2023-07-28 17:34:21 +0530

    Working demo, with off-target text insertions

--- ProofWidgets/Demos/MotivatedProof.lean
+++ ProofWidgets/Demos/MotivatedProof.lean
@@ -1,6 +1,9 @@
-import Lean.Meta.ExprLens
 import ProofWidgets.Component.HtmlDisplay
 import ProofWidgets.Component.Panel
+
+open Lean Server
+
+section Utils
 
 def Lean.Syntax.contains? (pos : String.Pos) (stx : Syntax) : Bool := Option.toBool do
   let ⟨start, stop⟩ ← stx.getRange?
@@ -13,61 +16,90 @@ def Lean.Syntax.Stack.findSmallest? (stack : Syntax.Stack) (p : Syntax → Bool)
 def Lean.Syntax.getHeadKind? (stx : Syntax) :=
   Syntax.getKind <$> stx.getHead?
 
+def getLineIndentation (line : String) : Nat :=
+  line |>.takeWhile (· ∈ [' ', '·', '{', '}']) |>.length
 
-open Lean Server
+end Utils
 
-def insertText (cmdStx : Syntax) (msg : String) (doc : FileWorker.EditableDocument) : RequestM Lsp.WorkspaceEdit := do
-  let .some pos := cmdStx.getTailPos? | panic! s!"Could not get range of {cmdStx}."
-  let text := doc.meta.text
-  let textEdit : Lsp.TextEdit :=
-    { range := { start := text.utf8PosToLspPos pos, «end» := text.utf8PosToLspPos pos },
-      newText := "\n    " ++ msg }
-  let textDocumentEdit : Lsp.TextDocumentEdit :=
-    { textDocument := { uri := doc.meta.uri, version? := doc.meta.version },
-      edits := #[textEdit] }
-  return .ofTextDocumentEdit textDocumentEdit
-
-structure InsertionButton where
-  label : String
-  text : String
-deriving RpcEncodable
-
-structure MotivatedProofPanelProps where
-  pos : Lsp.Position
-  buttons : Array InsertionButton
-deriving RpcEncodable
+section TextInsertion
 
 structure InsertionCommandProps where
   pos : Lsp.Position
   text : String
 deriving RpcEncodable
 
+structure InsertionButton where
+  label : String
+  text : String
+deriving RpcEncodable
+
+structure InsertionResponse where
+  edit : Lsp.WorkspaceEdit
+  newPos : Lsp.Position
+deriving RpcEncodable
+
+def insertText (pos : Lsp.Position) (msg : String) (doc : FileWorker.EditableDocument) :
+    RequestM InsertionResponse := do
+  let textEdit : Lsp.TextEdit :=
+    { range := { start := pos, «end» := pos },
+      newText := msg }
+  let textDocumentEdit : Lsp.TextDocumentEdit :=
+    { textDocument := { uri := doc.meta.uri, version? := doc.meta.version },
+      edits := #[textEdit] }
+  let edit := Lsp.WorkspaceEdit.ofTextDocumentEdit textDocumentEdit
+  return { edit := edit, newPos := ⟨pos.line + 1, pos.character⟩ }
+
 @[server_rpc_method]
-def makeInsertionCommand : InsertionCommandProps → RequestM (RequestTask Lsp.WorkspaceEdit)
+def makeInsertionCommand : InsertionCommandProps → RequestM (RequestTask InsertionResponse)
   | ⟨pos, text⟩ =>
     RequestM.withWaitFindSnapAtPos pos fun snap ↦ do
       let doc ← RequestM.readDoc
-      -- let fileMap := doc.meta.text
-      insertText snap.stx text doc
+      insertText pos text doc
+
+end TextInsertion
+
+namespace MotivatedProofInterface
+
+macro "◾" label:str " → " tac:tactic : term =>
+  let text : StrLit := Syntax.mkStrLit tac.raw.reprint.get!
+ `(term| InsertionButton.mk $label $text)
+
+end MotivatedProofInterface
+
+/-- The buttons that appear as proof-generating moves in the infoview panel. -/
+def tacticButtons : Array InsertionButton :=
+  #[ ◾ "Introduce variables into the context"  →  intros,
+     ◾       "Use function extensionality"     →  apply funext,
+     ◾           "Insert a sorry"              →  sorry,
+     ◾         "Simplify the target"           →  simp ]
+
+namespace MotivatedProofInterface
 
 open ProofWidgets
+open scoped Json Jsx
 
-def tacticButtons : Array InsertionButton :=
-  #[⟨"Introduce variables into the context", "intros"⟩, ⟨"Simplify the target", "simp"⟩]
+structure MotivatedProofPanelProps where
+  pos : Lsp.Position
+  buttons : Array InsertionButton
+deriving RpcEncodable
 
 @[widget_module]
-def InsertComponent : Component MotivatedProofPanelProps where
+def MotivatedProofPanel : Component MotivatedProofPanelProps where
   javascript := include_str "../../build/js/motivatedProofUI.js"
 
-open scoped Jsx in
-#html <InsertComponent pos={⟨58, 0⟩} buttons={tacticButtons} />
+syntax (name := motivatedProofMode) "motivated_proof" tacticSeq : tactic
 
--- syntax (name := motivatedProofMode) "motivated_proof" tacticSeq : tactic
+open Lean Elab Tactic in
+@[tactic motivatedProofMode]
+def motivatedProofImpl : Tactic
+  | stx@`(tactic| motivated_proof $tacs) => do
+    savePanelWidgetInfo stx ``MotivatedProofPanel do
+      return json% { buttons : $(← rpcEncode tacticButtons) }
+    evalTacticSeq tacs
+  | _ => throwUnsupportedSyntax
 
--- open Lean Elab Tactic in
--- @[tactic motivatedProofMode]
--- def motivatedProofImpl : Tactic
---   | stx@`(tactic| motivated_proof $tacs) => do
---     savePanelWidgetInfo stx _ _
---     evalTacticSeq tacs
---   | _ => throwUnsupportedSyntax
+end MotivatedProofInterface
+
+example : 1 = 1 := by
+  motivated_proof
+  sorry
