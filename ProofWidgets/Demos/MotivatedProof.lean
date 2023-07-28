34c4e416da7ddb87333569fd464f4450d90fce32
ART0 <18333981+0Art0@users.noreply.github.com>
2023-07-28 13:49:57 +0530

    The corresponding (messy) Lean code supporting an array of buttons

--- ProofWidgets/Demos/MotivatedProof.lean
+++ ProofWidgets/Demos/MotivatedProof.lean
@@ -2,7 +2,7 @@ import Lean.Meta.ExprLens
 import ProofWidgets.Component.HtmlDisplay
 import ProofWidgets.Component.Panel
 
-def Lean.Syntax.contains? (pos : String.Pos) (stx : Syntax)  : Bool := Option.toBool do
+def Lean.Syntax.contains? (pos : String.Pos) (stx : Syntax) : Bool := Option.toBool do
   let ⟨start, stop⟩ ← stx.getRange?
   guard <| start ≤ pos
   guard <| pos ≤ stop
@@ -17,76 +17,57 @@ def Lean.Syntax.getHeadKind? (stx : Syntax) :=
 open Lean Server
 
 def insertText (cmdStx : Syntax) (msg : String) (doc : FileWorker.EditableDocument) : RequestM Lsp.WorkspaceEdit := do
-  let .some pos := cmdStx.getTailPos? | panic! "could not get range"
+  let .some pos := cmdStx.getTailPos? | panic! s!"Could not get range of {cmdStx}."
   let text := doc.meta.text
-  let textEdit : Lsp.TextEdit := { range := { start := text.utf8PosToLspPos pos, «end» := text.utf8PosToLspPos pos }, newText := msg }
-  let textDocumentEdit :=
+  let textEdit : Lsp.TextEdit :=
+    { range := { start := text.utf8PosToLspPos pos, «end» := text.utf8PosToLspPos pos },
+      newText := "\n    " ++ msg }
+  let textDocumentEdit : Lsp.TextDocumentEdit :=
     { textDocument := { uri := doc.meta.uri, version? := doc.meta.version },
       edits := #[textEdit] }
   return .ofTextDocumentEdit textDocumentEdit
 
-structure InsertionProps where
-  pos : Lsp.Position
+structure InsertionButton where
   label : String
   text : String
 deriving RpcEncodable
 
+structure MotivatedProofPanelProps where
+  pos : Lsp.Position
+  buttons : Array InsertionButton
+deriving RpcEncodable
+
+structure InsertionCommandProps where
+  pos : Lsp.Position
+  text : String
+deriving RpcEncodable
+
 @[server_rpc_method]
-def makeInsertionCommand : InsertionProps → RequestM (RequestTask Lsp.WorkspaceEdit)
-  | ⟨pos, _, text⟩ =>
+def makeInsertionCommand : InsertionCommandProps → RequestM (RequestTask Lsp.WorkspaceEdit)
+  | ⟨pos, text⟩ =>
     RequestM.withWaitFindSnapAtPos pos fun snap ↦ do
       let doc ← RequestM.readDoc
+      -- let fileMap := doc.meta.text
       insertText snap.stx text doc
 
 open ProofWidgets
 
+def tacticButtons : Array InsertionButton :=
+  #[⟨"Introduce variables into the context", "intros"⟩, ⟨"Simplify the target", "simp"⟩]
+
 @[widget_module]
-def InsertComponent : Component InsertionProps where
+def InsertComponent : Component MotivatedProofPanelProps where
   javascript := include_str "../../build/js/motivatedProofUI.js"
 
 open scoped Jsx in
-#html <InsertComponent pos={⟨58, 0⟩} label={"Introduce variables into the context"} text={"Hello world"} />
+#html <InsertComponent pos={⟨58, 0⟩} buttons={tacticButtons} />
 
-#check RequestM
+-- syntax (name := motivatedProofMode) "motivated_proof" tacticSeq : tactic
 
-
-@[widget_module]
-def InsertPanel : Component PanelWidgetProps where
-  javascript := "
-import * as React from 'react'
-import { EditorContext, RpcContext, mapRpcError, useAsync } from '@leanprover/infoview'
-
-const e = React.createElement
-
-function ConvButton(pos) {
-  const rs = React.useContext(RpcContext)
-  const ec = React.useContext(EditorContext)
-  const st = useAsync(async () => {
-    return await rs.call('makeInsertionCommand', pos)
-  }, [])
-
-  function onClick() {
-    return void (async () => {
-      if (st.value) {
-        await ec.api.applyEdit(st.value.edit)
-      }
-    })()
-  }
-
-  const txt = st.value ? st.value.label : 'no insertion'
-
-  return st.state === 'resolved' ? e('button', {onClick}, txt)
-    : st.state === 'rejected' ? e('button', {style: {color: 'red'}}, mapRpcError(st.error).message)
-    : e('button', null, 'Loading..')
-}
-
-export default function(props) {
-  return (
-  <details open=\"true\">
-    <summary>Moves for constructing a well-motivated proof.</summary>
-    <div>
-      ConvButton(props.pos);
-    </div>
-  </details>);
-}
-  "
+-- open Lean Elab Tactic in
+-- @[tactic motivatedProofMode]
+-- def motivatedProofImpl : Tactic
+--   | stx@`(tactic| motivated_proof $tacs) => do
+--     savePanelWidgetInfo stx _ _
+--     evalTacticSeq tacs
+--   | _ => throwUnsupportedSyntax
