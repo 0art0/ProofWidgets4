faece067e57259ae1dc5c663b52a765b9e8ef0a2
0art0 <18333981+0art0@users.noreply.github.com>
2026-01-25 22:41:24 +0000

    feat: use CPS to write BuilderM to have a single createStatefulHtml
    Co-Authored By: Siddharth Bhat<siddu.druid@gmail.com>

--- ProofWidgets/Component/StatefulHtml.lean
+++ ProofWidgets/Component/StatefulHtml.lean
@@ -1,5 +1,7 @@
 import ProofWidgets
 
+set_option Elab.async false
+
 open Lean ProofWidgets Server Jsx Elab Command
 
 abbrev IOUnit := IO Unit
@@ -51,6 +53,22 @@ def InteractiveM.pure {α} (a : α) : InteractiveM α := .ofM do
  (← read).set (some a)
  return <div></div>
 
+def InteractiveM.cast (β : Type) (x : InteractiveM β) : InteractiveM β := x
+
+/-
+/-- Close the loop in the diagram, feeding back 'x.run' until we get a value. -/
+def InteractiveM.loop (x : InteractiveM α) : InteractiveM α := .ofM do
+  let switch : Mutable (Option α) ← createMutableState none
+  -- let state ← read
+  match ← switch.get with
+  | none => createStatefulHtml do x.run switch
+  | some a => (InteractiveM.pure a).run
+
+def InteractiveM.seq (ma : InteractiveM Unit) (mb : InteractiveM β) : InteractiveM β := .ofM do
+  let _ ← (InteractiveM.loop ma).run .none
+  return b
+-/
+
 def InteractiveM.bind {α β} (x : InteractiveM α) (f : α → InteractiveM β) : InteractiveM β := .ofM do
   let switch : Mutable (Option α) ← createMutableState none
   let state ← read
@@ -72,15 +90,114 @@ def showHtml (html : Html) : InteractiveM Unit := .ofM do
 
 def runInteractiveM {α} (m : InteractiveM α) : IO Html := do
   let state : Mutable (Option α) ← createMutableState none
+  -- createStatefulHtml <|
   m.run state
+
 
 #html show IO _ from do
   let count ← createMutableState (init := 0)
-  createStatefulHtml <|
-    return <div>
+  let html : IO.Ref (IO Html) ← IO.mkRef do return (
+    <div>
       <Button label={s!"Click me! {← count.get}"} onClick={← WithRpcRef.mk (count.modify (· + 1))} />
       <p>Count: {.text s!"{← count.get}"}</p>
+    </div>)
+  createStatefulHtml <| (← html.get)
+
+#html show IO _ from do
+  let count ← createMutableState (init := 0)
+  let html : IO Html := do pure (
+    <div>
+      <Button label={s!"Click me! {← count.get}"} onClick={← WithRpcRef.mk (count.modify (· + 1))} />
+      <p>Count: {.text s!"{← count.get}"}</p>
+    </div>)
+  createStatefulHtml  html
+
+
+-- Hom(Hom(-, r), r)
+def Cont (r a : Type) := (a → r) → r
+
+def Cont.pure {r α} (a : α) : Cont r α := fun k => k a
+
+def Cont.bind {r α β} (x : Cont r α) (f : α → Cont r β) : Cont r β :=
+  fun k => x (fun a => f a k)
+
+def Cont.map {r α β} (x : Cont r α) (f : α → β) : Cont r β :=
+  x.bind (fun a => Cont.pure (f a))
+
+def Cont.run {r} (x : Cont r r) : r :=
+  x id
+
+instance : Monad (Cont r) where
+  pure := Cont.pure
+  bind := Cont.bind
+
+
+def Cont.runUnit {r} (x : Cont r r) : r :=
+  x id
+
+def Cont' (r : Type) := (a : Type) → (a → r) → r
+
+def Cont'.run {r a} (x : Cont' r) (k : a → r) : r :=
+  x _ k
+
+def Cont'.pure {r} (val : r) : Cont' r := fun _alpha _k => val
+
+def Cont'.bind {r } (x : Cont' r) (f : r → Cont' s) : Cont' s :=
+  f (x.run id)
+
+def Cont'.map {r s} (x : Cont' r) (f : r → s) : Cont' s :=
+  x.bind (fun a => Cont'.pure (f a))
+
+instance : Monad (Cont') where
+  pure := Cont'.pure
+  bind := Cont'.bind
+
+def BuilderM : Type := Html × IO.Ref (Html) → IO Html
+
+def BuilderM.finish (x : BuilderM) : IO Html := do
+  let htmlRef : IO.Ref (Html) ← IO.mkRef (<div></div>)
+  let html ← x (<div></div>, htmlRef)
+  htmlRef.set html
+  createStatefulHtml <| htmlRef.get
+
+def BuilderM.button (label : String) : BuilderM :=
+  fun (next, ref) => do
+    return <div>
+      <Button label={label} onClick={← WithRpcRef.mk do ref.set next} />
     </div>
+
+def BuilderM.seq (y : BuilderM) (x : BuilderM) : BuilderM :=
+  fun s => do
+    let h1 ← x s
+    y (h1, s.2)
+
+#html show IO _ from BuilderM.finish <|
+  BuilderM.button "Click me!" |>.seq
+  (BuilderM.button "And again!" |>.seq
+    (BuilderM.button "One more time!"))
+
+
+#html show IO _ from do
+  let count ← createMutableState (init := 0)
+  let html : IO.Ref (Html) ← IO.mkRef (<div></div>)
+  let h0 : (Html) := (<p> That's all, folks! </p>)
+  let h1 : (Html) := (<Button label="one more time" onClick={← WithRpcRef.mk do count.modify (· + 1); html.set ( h0)} />)
+  let h2 : (Html) := (<Button label="and again" onClick={← WithRpcRef.mk do count.modify (· + 1); html.set ( h1)} />)
+  let h3 : (Html) := (<Button label="click me!" onClick={← WithRpcRef.mk do count.modify (· + 1); html.set ( h2)} />)
+  html.set h3
+  createStatefulHtml <| return <div>{(← html.get)}{.text s!"XXCount: {← count.get}"}</div>
+
+
+#html show IO _ from do
+  let count ← createMutableState (init := 0)
+  let html : IO.Ref (IO Html) ← IO.mkRef do return (<div></div>)
+  let h0 : (IO Html) := return (<p> That's all, folks! </p>)
+  let h1 : (IO Html) := return (<Button label="one more time" onClick={← WithRpcRef.mk do count.modify (· + 1); html.set ( h0)} />)
+  let h2 : (IO Html) := return (<Button label="and again" onClick={← WithRpcRef.mk do count.modify (· + 1); html.set ( h1)} />)
+  let h3 : (IO Html) := return (<Button label="click me!" onClick={← WithRpcRef.mk do count.modify (· + 1); html.set ( h2)} />)
+  html.set h3
+  createStatefulHtml <| return <div>{← (← html.get)}{.text s!"XXCount: {← count.get}"}</div>
+
 
 #html show IO _ from do
   createStatefulHtml <| runInteractiveM do
