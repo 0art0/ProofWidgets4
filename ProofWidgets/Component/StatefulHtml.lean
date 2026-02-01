fe78a08b52e7afa80107837078a8bdbe61bce5ed
0art0 <18333981+0art0@users.noreply.github.com>
2026-02-01 21:51:11 +0000

    Some minimizations

--- ProofWidgets/Component/StatefulHtml.lean
+++ ProofWidgets/Component/StatefulHtml.lean
@@ -136,130 +136,117 @@ instance : Monad (Cont r) where
 
 
 
--- (a -> r) -> r
-def Cont' (a : Type) := ({r : Type} → (a → r) → r)
+-- -- (a -> r) -> r
+-- def Cont' (a : Type) := ({r : Type} → (a → r) → r)
 
-def Cont'.run {r} (x : Cont' a) (k : a → r) : r :=
-  x k
+-- def Cont'.run {r} (x : Cont' a) (k : a → r) : r :=
+--   x k
 
-def Cont'.pure {r} (val : r) : Cont' r := fun k => k val
+-- def Cont'.pure {r} (val : r) : Cont' r := fun k => k val
 
-def Cont'.bind (ka : Cont' a) (a2kb : a → Cont' b) : Cont' b :=
-  a2kb (ka.run id)
+-- def Cont'.bind (ka : Cont' a) (a2kb : a → Cont' b) : Cont' b :=
+--   a2kb (ka.run id)
 
-def Cont'.map {r s} (x : Cont' r) (f : r → s) : Cont' s :=
-  x.bind (fun a => Cont'.pure (f a))
+-- def Cont'.map {r s} (x : Cont' r) (f : r → s) : Cont' s :=
+--   x.bind (fun a => Cont'.pure (f a))
 
-instance : Monad (Cont') where
-  pure := Cont'.pure
-  bind := Cont'.bind
+-- instance : Monad (Cont') where
+--   pure := Cont'.pure
+--   bind := Cont'.bind
 
+def Builder := Html × IO.Ref Html → IO Html
 
-
-def BuilderM (α : Type) : Type := Html × IO.Ref (Html) → IO (Html × α)
-
-def BuilderM.finish (x : BuilderM Unit) : IO Html := do
-  let htmlRef : IO.Ref (Html) ← IO.mkRef (<div></div>)
-  let (html, _) ← x (<div></div>, htmlRef)
+def Builder.finish (x : Builder) : IO Html := do
+  let htmlRef : IO.Ref Html ← IO.mkRef (<div></div>)
+  let html ← x (<div></div>, htmlRef)
   htmlRef.set html
   createStatefulHtml <| htmlRef.get
 
-def BuilderM.button (label : String) : BuilderM Unit :=
+def Builder.button (label : String) : Builder :=
   fun (next, ref) => do
-    return (<div>
+    return <div>
       <Button label={label} onClick={← WithRpcRef.mk do ref.set next} />
-    </div>, ())
+    </div>
 
-def BuilderM.getBool (statement : String) (b : Bool -> BuilderM Unit) : BuilderM Unit :=
+def Builder.getBool (statement : String) (b : Bool -> Builder) : Builder :=
   fun (next, ref) => do
     let resultRef : IO.Ref (Option Bool) ← IO.mkRef none
     let html :=
       <div>
         <p>{.text statement}</p>
-        <Button label="True" onClick={← WithRpcRef.mk do resultRef.set true; ref.set (← b true (next, ref)).fst } />
-        <Button label="False" onClick={← WithRpcRef.mk do resultRef.set false; ref.set (← b false (next, ref)).fst } />
+        <Button label="True" onClick={← WithRpcRef.mk do resultRef.set true; ref.set (← b true (next, ref)) } />
+        <Button label="False" onClick={← WithRpcRef.mk do resultRef.set false; ref.set (← b false (next, ref)) } />
       </div>
-    return (html, ())
+    return html
 
-def BuilderM.getBoolK (label : String) : Cont (BuilderM Unit) Bool := BuilderM.getBool label
+def Builder.getBoolK (label : String) : Cont Builder Bool := Builder.getBool label
 
-def BuilderM.seq' (y : BuilderM β) (x : β → BuilderM α) : BuilderM α :=
+def Builder.seq (y : Builder) (x : Builder) : Builder :=
   fun s => do
-    let (html, betaval) ← y s
-    (x betaval) (html, s.2)
+    let html ← x s
+    y (html, s.2)
+
+def Builder.buttonUnit (label : String) (rest : Unit -> Builder) : Builder :=
+  Builder.seq (rest ()) (Builder.button label)
+
+def Builder.buttonK (label : String) : Cont Builder Unit :=
+  Builder.buttonUnit label
+
+def Builder.empty : Builder :=
+  fun (next, _ref) => return next
+
+def Builder.runK (x : Cont Builder Unit) : Builder :=
+  x.run (fun _ => Builder.empty)
+
+#html show IO _ from Builder.finish <|
+  Builder.button "Click me!" |>.seq
+  (Builder.button "And again!" |>.seq
+    (Builder.button "One more time!"))
+
+#html show IO _ from Builder.finish <| Builder.runK <| do
+  let b <- Builder.getBoolK "foo"
+  if b then Builder.buttonK "Click me!" else Builder.buttonK "And again!"
 
 
-def BuilderM.buttonUnit (label : String) (rest : Unit -> BuilderM Unit) : BuilderM Unit :=
-  BuilderM.seq' (rest ()) (fun () => BuilderM.button label)
-
-def BuilderM.buttonK (label : String) : Cont (BuilderM Unit) Unit :=
-  BuilderM.buttonUnit label
+  -- (Builder.button "And again!" |>.seq
+    -- (Builder.button "One more time!"))
 
 
+abbrev StupidM (α : Type):= StateM (Builder → Builder) α
 
-
-def BuilderM.empty : BuilderM Unit :=
-  fun (next, _ref) => return (next, ())
-
-
-def BuilderM.runK (x : Cont (BuilderM Unit) Unit) : BuilderM Unit :=
-  x.run (fun _ => BuilderM.empty)
-
-def BuilderM.seq (y : BuilderM Unit) (x : BuilderM Unit) : BuilderM Unit :=
-  fun s => do
-    let (h1, _) ← x s
-    y (h1, s.2)
-
-#html show IO _ from BuilderM.finish <| BuilderM.runK <| do
-  let b <- BuilderM.getBoolK "foo"
-  if b then BuilderM.buttonK "Click me!" else BuilderM.buttonK "And again!"
-
-
-  -- (BuilderM.button "And again!" |>.seq
-    -- (BuilderM.button "One more time!"))
-
-
-abbrev StupidM (α : Type):= StateM (BuilderM Unit → BuilderM Unit) α
 def StupidM.button (label : String) : StupidM Unit := do
-  modify fun b => b ∘ ((BuilderM.button label).seq)
+  modify fun b => b ∘ (Builder.button label).seq
 
 def StupidM.finish (x : StupidM Unit) : IO Html :=
   let (_, f) := x.run id
-  BuilderM.finish (f BuilderM.empty)
+  Builder.finish (f Builder.empty)
 
 
 #html show IO _ from StupidM.finish do
-  StupidM.button "Click me!"
-  StupidM.button "And again!"
-  StupidM.button "One more time!"
-
-
-#html show IO _ from BuilderM.finish <|
-  BuilderM.button "Click me!" |>.seq
-  (BuilderM.button "And again!" |>.seq
-    (BuilderM.button "One more time!"))
-
+  .button "Click me!"
+  .button "And again!"
+  .button "One more time!"
 
 #html show IO _ from do
   let count ← createMutableState (init := 0)
-  let html : IO.Ref (Html) ← IO.mkRef (<div></div>)
-  let h0 : (Html) := (<p> That's all, folks! </p>)
-  let h1 : (Html) := (<Button label="one more time" onClick={← WithRpcRef.mk do count.modify (· + 1); html.set ( h0)} />)
-  let h2 : (Html) := (<Button label="and again" onClick={← WithRpcRef.mk do count.modify (· + 1); html.set ( h1)} />)
-  let h3 : (Html) := (<Button label="click me!" onClick={← WithRpcRef.mk do count.modify (· + 1); html.set ( h2)} />)
+  let html : IO.Ref Html ← IO.mkRef (<div></div>)
+  let h0 : Html := (<p> That's all, folks! </p>)
+  let h1 : Html := (<Button label="one more time" onClick={← WithRpcRef.mk do count.modify (· + 1); html.set ( h0)} />)
+  let h2 : Html := (<Button label="and again" onClick={← WithRpcRef.mk do count.modify (· + 1); html.set ( h1)} />)
+  let h3 : Html := (<Button label="click me!" onClick={← WithRpcRef.mk do count.modify (· + 1); html.set ( h2)} />)
   html.set h3
   createStatefulHtml <| return <div>{(← html.get)}{.text s!"XXCount: {← count.get}"}</div>
 
 
-#html show IO _ from do
-  let count ← createMutableState (init := 0)
-  let html : IO.Ref (IO Html) ← IO.mkRef do return (<div></div>)
-  let h0 : (IO Html) := return (<p> That's all, folks! </p>)
-  let h1 : (IO Html) := return (<Button label="one more time" onClick={← WithRpcRef.mk do count.modify (· + 1); html.set ( h0)} />)
-  let h2 : (IO Html) := return (<Button label="and again" onClick={← WithRpcRef.mk do count.modify (· + 1); html.set ( h1)} />)
-  let h3 : (IO Html) := return (<Button label="click me!" onClick={← WithRpcRef.mk do count.modify (· + 1); html.set ( h2)} />)
-  html.set h3
-  createStatefulHtml <| return <div>{← (← html.get)}{.text s!"XXCount: {← count.get}"}</div>
+-- #html show IO _ from do
+--   let count ← createMutableState (init := 0)
+--   let html : IO.Ref (IO Html) ← IO.mkRef do return (<div></div>)
+--   let h0 : IO Html := return (<p> That's all, folks! </p>)
+--   let h1 : IO Html := return (<Button label="one more time" onClick={← WithRpcRef.mk do count.modify (· + 1); html.set ( h0)} />)
+--   let h2 : IO Html := return (<Button label="and again" onClick={← WithRpcRef.mk do count.modify (· + 1); html.set ( h1)} />)
+--   let h3 : IO Html := return (<Button label="click me!" onClick={← WithRpcRef.mk do count.modify (· + 1); html.set ( h2)} />)
+--   html.set h3
+--   createStatefulHtml <| return <div>{← (← html.get)}{.text s!"XXCount: {← count.get}"}</div>
 
 
 #html show IO _ from do
